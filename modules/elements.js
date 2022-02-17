7557ff2628db0504051294f07aab7a172ec0a1d6
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-02-17 11:15:55 -0300

    New Layout tests and ajustments

---
+++ modules/elements.js
@@ -0,0 +1,20 @@
+export const container = document.querySelector('.books-list-sec');
+export const bookaddsection = document.querySelector('.books-add-sec');
+export const contactussection = document.querySelector('.contact-us-sec');
+export const title = document.querySelector('.book-title');
+export const author = document.querySelector('.book-author');
+export const errorlable = document.querySelector('.book-fields-valid');
+
+// Top Menu Elements
+export const listNav = document.querySelector('.top-menu-item-list');
+export const addNewNav = document.querySelector('.top-menu-item-add');
+export const contactNav = document.querySelector('.top-menu-item-contact');
+
+// Middle of the page sections
+export const listBox = document.querySelector('.books-list-sec');
+export const addBox = document.querySelector('.books-add-sec');
+
+export const contactBox = document.querySelector('.contact-us-sec');
+
+// Date & Time display
+export const currentdate = document.querySelector('.date-time-display');
