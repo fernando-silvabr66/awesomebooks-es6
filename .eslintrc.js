9b3bf41a92381269921b0762c1d265d5ff37b25a
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-02-17 14:18:02 -0300

    update .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
         "eslint:recommended",
         "plugin:react/recommended"
     ],
-    "parser": "babel-eslint",
+    "parser": "babel-eslint"
     },
     "parserOptions": {
         "ecmaFeatures": {
